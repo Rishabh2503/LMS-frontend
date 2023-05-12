95b7c011227988ed0378750f60b461cd6ced4537
Rishabh2503 <gupta.rish2501@gmail.com>
2023-05-13 02:13:37 +0530

    Updated Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import "../Style/Login.css"
-import Loginsvg from "../assesst/illustrations/Login.svg"
+import reactangle from "../assesst/illustrations/Login/reactangle1.svg"
+import reactangle2 from "../assesst/illustrations/Login/Rectangle2.svg"
+import Rectangle3 from "../assesst/illustrations/Login/Rectangle3.svg"
+import reactangle4 from "../assesst/illustrations/Login/reactangle4.svg"
 function LoginForm() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -19,13 +22,18 @@ function LoginForm() {
 
   return (
     <header>
+    <div className='reactangle'>
+    <img src={reactangle} alt="reactangle" />
+    </div>
+   <div className='reactangle2'> <img src={reactangle2} alt="Rectangle2" /></div>
+   <div className='reactangle4'> <img src={reactangle4} alt="reactangle4" /></div>
      <div className='login-box'>
     <div className="login-form__container">
-      <h2 className="login-form__title">Login</h2>
+      <h2 className="login-form__title">Welcome</h2>
      
       <form onSubmit={handleSubmit}>
         <div className="login-form__input-container">
-          <label htmlFor="email">Email:</label>
+          <label htmlFor="email">Username</label>
           <input
             type="email"
             id="email"
@@ -36,7 +44,7 @@ function LoginForm() {
           />
         </div>
         <div className="login-form__input-container">
-          <label htmlFor="password">Password:</label>
+          <label htmlFor="password">Password</label>
           <input
             type="password"
             id="password"
@@ -50,10 +58,6 @@ function LoginForm() {
           Login
         </button>
       </form>
-      
-    </div>
-    <div className='login-svg'>
-         <img src={Loginsvg} alt="login" />
       </div>
     </div>
     </header>
